732d8aa5693eb7ad4f8cbbf632447701d864b8e7
Samuel Brandão <samuelbrandao08@gmail.com>
2024-10-09 14:44:39 -0300

    Config: Configuração do backend com Spring Boot e conexão com o postgre database.

---
+++ corretor-de-provas/src/main/java/com/samuelbrandao/corretor_de_provas/CorretorDeProvasApplication.java
@@ -0,0 +1,13 @@
+package com.samuelbrandao.corretor_de_provas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorretorDeProvasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorretorDeProvasApplication.class, args);
+	}
+
+}
---
+++ corretor-de-provas/src/test/java/com/samuelbrandao/corretor_de_provas/CorretorDeProvasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuelbrandao.corretor_de_provas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorretorDeProvasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
